0cb9bff9df9bfbb828489d32e2b30dc220aaaa00
gbalaji <gadi.balaji@epsilon.com>
2024-01-07 15:53:51 +0530

    pushing basic terraform file

--- ec2-instance/ec2-instance.tf
+++ ec2-instance/ec2-instance.tf
@@ -6,17 +6,21 @@ required_providers {
       }
 }
 }
-
 ##provider block
 provider "aws" {
-
     profile = "default"
-    region = "us-west-1"
+    region = "us-east-1"
 }
-
 ##resource block
 resource aws_instance "ec2-demo-balaji" {
-
     ami = "ami-03265a0778a880afb"
     instance_type = "t3.small"
 }
+
+/* terraform commands
+terraform init -> to intialize the terrform configuration files.check 
+terraform validate -> to validate the configuration files
+terraform plan -> to generate the plan what resources are going to create it
+terraform apply -auto-approve -> it will create resource here ec2 instance in aws cloud
+terraform destroy -auto-approve -> It destroy ec2 instance
+*/
