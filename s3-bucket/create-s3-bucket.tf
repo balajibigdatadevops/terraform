d41155f185ecf6056242687bc4f29cf5bf40be18
gbalaji <gadi.balaji@epsilon.com>
2024-01-07 17:00:00 +0530

    pushing basic terraform file

--- s3-bucket/create-s3-bucket.tf
+++ s3-bucket/create-s3-bucket.tf
@@ -31,7 +31,7 @@ resource "random_pet" "petname" {
 # Resource Block: Create AWS S3 Bucket
 resource "aws_s3_bucket" "sample" {
   bucket = random_pet.petname.id
-  acl    = "public-read"
+   aws_s3_bucket_acl  = "public-read"
   #region = "us-east-1"  # Comment this if we are going to use AWS Provider v3.x version
 
 }
