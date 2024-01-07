5a1eeb24e72f3843c8e79d258a21c128847807c1
gbalaji <gadi.balaji@epsilon.com>
2024-01-07 16:05:17 +0530

    pushing terraform block

--- terraform-block/terraform-block.tf
+++ terraform-block/terraform-block.tf
@@ -5,10 +5,6 @@ terraform {
         source = "hashicorp/aws"
         version = "~> 3.0"
        }
-       random = {
-        source = "hashicorp/aws"
-        version = "3.0.1"
-       }
     }
 }
 
